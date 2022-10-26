28d4eab9f9f9de5edef0e9a2e0b46829e43cc9fb
Александр Ситников <you@example.com>
2022-10-26 13:22:23 +0500

    задачка 4.2

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const nameProduct = "смеситель";
+const quantity = 2;
+const category = "кухонный";
+const price = 7300;
+
+console.log(nameProduct);
+console.log("Итоговая сумма товара:", quantity*price +` руб.`);
